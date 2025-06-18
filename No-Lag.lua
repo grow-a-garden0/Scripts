f66217625f698f4caaf5154e6a5b401f80870e0a
grow-a-garden0 <ranieltmolinas@gmail.com>
2025-06-18 19:48:58 +0800

    Update No-Lag.lua

--- No-Lag.lua
+++ No-Lag.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/blank2xp/auza-gen-repo/main/auza.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/grow-a-garden0/Script/refs/heads/main/9d4bf24dfbfbb629b1fca78990796bd7.txt"))()
